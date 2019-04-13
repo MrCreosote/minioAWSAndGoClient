63ef835793083aecf594ab5674d2a6d8233fd1f7
Gavin <gaprice@lbl.gov>
2019-04-12 17:40:01 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/mrcreosote/gofun
+
+go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.19.11
+	github.com/go-ini/ini v1.42.0 // indirect
+	github.com/minio/minio-go v6.0.14+incompatible
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20190411191339-88737f569e3a // indirect
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
+)
---
+++ miniotest.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/minio/minio-go"
+)
+
+func main() {
+	endpoint := "localhost:9000"
+	accessKeyID := "9V25FKN0JY7IQZUW85RH"
+	secretAccessKey := "wckkTpC3lZ5QYqY0jIJXFJ6XEUsmD1nBCZK7vmva"
+	useSSL := false
+
+	bucket := "mybukkit"
+	region := "us-west-1"
+
+	objectName := "somefile"
+	filePath := "/home/crushingismybusiness/examples.desktop"
+	contentType := "text/plain"
+
+	doAWS(endpoint, accessKeyID, secretAccessKey, useSSL, bucket, region, objectName, filePath,
+		contentType)
+	//doMinio(endpoint, accessKeyID, secretAccessKey, useSSL, bucket, region, objectName,
+	//	filePath, contentType)
+}
+
+func doAWS(
+	endpoint string,
+	accessKeyID string,
+	secretAccessKey string,
+	useSSL bool,
+	bucket string,
+	region string,
+	objectName string,
+	filePath string,
+	contentType string) {
+
+	trueref := true
+
+	sess := session.Must(session.NewSession())
+	creds := credentials.NewStaticCredentials(accessKeyID, secretAccessKey, "")
+	svc := s3.New(sess, &aws.Config{
+		Credentials:      creds,
+		Endpoint:         &endpoint,
+		Region:           &region,
+		DisableSSL:       &trueref,  // detect correct setting based on url prefix, warn for http
+		S3ForcePathStyle: &trueref}) // minio pukes otherwise
+
+	input := &s3.CreateBucketInput{Bucket: aws.String(bucket)}
+
+	result, err := svc.CreateBucket(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeBucketAlreadyExists:
+				fmt.Println(s3.ErrCodeBucketAlreadyExists, aerr.Error())
+			case s3.ErrCodeBucketAlreadyOwnedByYou:
+				// should keep going here
+				fmt.Println(s3.ErrCodeBucketAlreadyOwnedByYou, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+
+	uploader := s3manager.NewUploaderWithClient(svc)
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("failed to open file %q, %v\n", filePath, err)
+		return
+	}
+
+	// Upload the file to S3.
+	objresult, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(objectName),
+		Body:   f,
+	})
+	if err != nil {
+		fmt.Printf("failed to upload file, %v\n", err)
+		return
+	}
+	fmt.Printf("file uploaded to, %s\n", objresult.Location)
+
+}
+
+func doMinio(
+	endpoint string,
+	accessKeyID string,
+	secretAccessKey string,
+	useSSL bool,
+	bucket string,
+	location string,
+	objectName string,
+	filePath string,
+	contentType string) {
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("%#v\n", minioClient) // minioClient is now setup
+
+	err = minioClient.MakeBucket(bucket, location)
+	if err != nil {
+		// Check to see if we already own this bucket (which happens if you run this twice)
+		exists, err := minioClient.BucketExists(bucket)
+		if err == nil && exists {
+			log.Printf("We already own %s\n", bucket)
+		} else {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Printf("Successfully created %s\n", bucket)
+	}
+
+	// Upload the zip file
+
+	// Upload the zip file with FPutObject
+	n, err := minioClient.FPutObject(bucket, objectName, filePath,
+		minio.PutObjectOptions{ContentType: contentType})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", objectName, n)
+}
